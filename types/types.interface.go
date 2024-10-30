be0fef20729b09b8bbc9327a2f73a1eb37982499
QuocAnh189 <anhquoc18092003@gmail.com>
2024-10-30 11:57:09 +0700

    setup types

---
+++ types/types.interface.go
@@ -0,0 +1,20 @@
+package types
+
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id int) (*User, error)
+	CreateUser(User) error
+}
+
+type ProductStore interface {
+	GetProductByID(id int) (*Product, error)
+	GetProductsByID(ids []int) ([]Product, error)
+	GetProducts() ([]*Product, error)
+	CreateProduct(CreateProductPayload) error
+	UpdateProduct(Product) error
+}
+
+type OrderStore interface {
+	CreateOrder(Order) (int, error)
+	CreateOrderItem(OrderItem) error
+}
---
+++ types/types.struct.go
@@ -0,0 +1,71 @@
+package types
+
+import (
+	"time"
+)
+
+type User struct {
+	ID 		    int 		`json:"id`
+	FirstName   string	  	`json:"first_name"`
+	LastName    string      `json:"last_name"`
+	Email       string      `json:"email"`
+	Password    string 	  	`json:"-"`
+	CreatedAt   time.Time   `json:"created_at"`
+}
+
+type Product struct {
+	ID		    int		  	`json:"id"`
+	Name	    string	  	`json:"name"`
+	Description	string	  	`json:"description"`
+	Image		string	  	`json:"image"`
+	Price       float64   	`json:"price"`
+	Quantity    int       	`json:"quantity"`
+	CreatedAt   time.Time 	`json:"created_at"`
+}
+
+type CartCheckoutItem struct {
+	ProductID 	int 	    `json:"productID"`
+	Quantity  	int 	    `json:"quantity"`
+}
+
+type Order struct {
+	ID          int         `json:"id"`
+	UserID      int         `json:"userID"`
+	Total       float64     `json:"total"`
+	Status      string      `json:"status"`
+	Address     string      `json:"address"`
+	CreatedAt   time.Time   `json:"created_at"`
+}
+
+type OrderItem struct {
+	ID        	int       	`json:"id"`
+	OrderID   	int       	`json:"orderID"`
+	ProductID 	int       	`json:"productID"`
+	Quantity  	int       	`json:"quantity"`
+	Price     	float64   	`json:"price"`
+	CreatedAt 	time.Time 	`json:"created_at"`
+}
+
+type RegisterUserPayload struct {
+	FirstName 	string 		`json:"firstName" validate:"required"`
+	LastName  	string 		`json:"lastName" validate:"required"`
+	Email     	string 		`json:"email" validate:"required,email"`
+	Password  	string 		`json:"password" validate:"required,min=3,max=130"`
+}
+
+type LoginUserPayload struct {
+	Email    	string 		`json:"email" validate:"required,email"`
+	Password 	string 		`json:"password" validate:"required"`
+}
+
+type CreateProductPayload struct {
+	Name        string  	`json:"name" validate:"required"`
+	Description string  	`json:"description"`
+	Image       string  	`json:"image"`
+	Price       float64 	`json:"price" validate:"required"`
+	Quantity    int     	`json:"quantity" validate:"required"`
+}
+
+type CartCheckoutPayload struct {
+	Items []CartCheckoutItem `json:"items" validate:"required"`
+}
